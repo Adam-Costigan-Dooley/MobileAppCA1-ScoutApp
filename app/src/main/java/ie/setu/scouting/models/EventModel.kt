9ac0beae011692b428a1a7565b392eabf32b764e
Adam-Costigan-Dooley <116007719+Adam-Costigan-Dooley@users.noreply.github.com>
2025-10-25 19:18:36 +0100

    V2.0: Added volunteer requirements to event planner

--- app/src/main/java/ie/setu/scouting/models/EventModel.kt
+++ app/src/main/java/ie/setu/scouting/models/EventModel.kt
@@ -2,5 +2,7 @@ package ie.setu.scouting.models
 
 data class EventModel(
     var title: String = "",
-    var description: String = ""
+    var description: String = "",
+    var leadersNeeded: Int = 0,
+    var parentVolunteersAllowed: Boolean = false
 )
